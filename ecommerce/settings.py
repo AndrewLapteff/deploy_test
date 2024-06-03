ce9ad1d37237f57847ed57b352319ce7d75e5e6a
Andrew Lapteff <this.is.amigogo@gmail.com>
2024-06-03 19:51:47 +0300

    third commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-6rzc(^-j%6%1y1eclmy-t@f+$&q8gbpsi*07438u(@3z8gn3$b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["web-production-10c0.up.railway.app"]
+ALLOWED_HOSTS = ["web-production-10c0.up.railway.app", "localhost"]
 
 
 # Application definition
